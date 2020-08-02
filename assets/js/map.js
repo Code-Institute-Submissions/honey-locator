66c75e712d28a22deec784d667a58b5de5d40a2d
irahbt <irah.bt@gmail.com>
2020-08-02 15:04:29 +0000

    Add geolocator with event listener map.js, add corresponding button index.html

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,7 +1,7 @@
 // initilise Map
 
 function initMap() {
-  const map = new google.maps.Map(document.getElementById('map'), {
+  const map = new google.maps.Map(document.getElementById("map"), {
     zoom: 5.5,
     center: { lat: 55.08122, lng: -3.016536 },
     mapTypeControl: false,
@@ -9,28 +9,28 @@ function initMap() {
 
 // set markers and clusters
 
-  map.data.loadGeoJson('assets/js/honey-data.json', null, function (features) {
+  map.data.loadGeoJson("assets/js/honey-data.json", null, function (features) {
     markers = features.map(function (feature) {
       let marker = new google.maps.Marker({
         position: feature.getGeometry().get(0),
         icon: {
-          url: `assets/images/icon_${feature.getProperty('category')}.jpg`,
+          url: `assets/images/icon_${feature.getProperty("category")}.jpg`,
           scaledSize: new google.maps.Size(50, 50),
         },
       });
 
 // add mouseover
 
-      marker.addListener('mouseover', function () {
+      marker.addListener("mouseover", function () {
         marker.setIcon({
-          url: `assets/images/icon_${feature.getProperty('category')}.jpg`,
+          url: `assets/images/icon_${feature.getProperty("category")}.jpg`,
           scaledSize: new google.maps.Size(60, 60),
         });
       });
 
-      marker.addListener('mouseout', function () {
+      marker.addListener("mouseout", function () {
         marker.setIcon({
-          url: `assets/images/icon_${feature.getProperty('category')}.jpg`,
+          url: `assets/images/icon_${feature.getProperty("category")}.jpg`,
           scaledSize: new google.maps.Size(50, 50),
         });
       });
@@ -39,15 +39,15 @@ function initMap() {
 
   infowindow = new google.maps.InfoWindow();
 
-      marker.addListener('click', function () {
+      marker.addListener("click", function () {
         let feat = feature;
-        let name = feat.getProperty('name');
-        let phone = feat.getProperty('phone');
-        let website = feat.getProperty('website');
+        let name = feat.getProperty("name");
+        let phone = feat.getProperty("phone");
+        let website = feat.getProperty("website");
         let position = feat.getGeometry().get();
         let html = `<b> ${name}</b><br>
-        <i class='fas fa-phone-alt icon-color' alt='phone'></i> ${phone}
-        <br><i class='fas fa-globe icon-color' alt='globe'></i> <a class='website' target='_blank' href=' ${website}'>link</a>`;
+        <i class="fas fa-phone-alt icon-color" alt="phone"></i> ${phone}
+        <br><i class="fas fa-globe icon-color" alt="globe"></i> <a class="website" target="_blank" href=" ${website}">link</a>`;
         infowindow.setContent(html);
         infowindow.open(map, marker);
       });
@@ -55,10 +55,50 @@ function initMap() {
     });
     let markerCluster = new MarkerClusterer(map, markers, {
       imagePath:
-        'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m',
+        "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m",
     });
   });
 
-  map.data.setMap(null);
-  
+    map.data.setMap(null);
+
+     //geolocator
+
+  let locatorBTN = document.getElementById("locatorBTN");
+  locatorBTN.addEventListener("click", function () {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+          };
+          infowindow.setPosition(pos);
+          infowindow.setContent(
+            "<p class = 'general-text'>Your location <img src ='assets/images/icon_Online.jpg' class='icon' alt='logo'></p>"
+          );
+          infowindow.open(map);
+          map.setCenter(pos);
+        },
+        () => {
+          handleLocationError(true, infowindow, map.getCenter());
+        }
+      );
+    } else {
+      // Browser doesn't support Geolocation
+      handleLocationError(false, infoWindow, map.getCenter());
+    }
+  });
+
+  function handleLocationError(browserHasGeolocation, infowindow, pos) {
+    infowindow.setPosition(pos);
+    infowindow.setContent(
+      browserHasGeolocation
+        ? "Error: The Geolocation service failed."
+        : "Error: Your browser doesn't support geolocation."
+    );
+    infowindow.open(map);
+  }
+
 }
+
+
