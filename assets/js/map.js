f2c5acf6173de117bf52fe44727a53c91d2f0d47
irahbt <irah.bt@gmail.com>
2020-08-03 09:40:16 +0000

    Add place autocomplete and directions

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,3 +1,10 @@
+
+
+/*let geoJSONObject = '{"type": "FeatureCollection", "features": [{"geometry": {"coordinates": [-5.851389, 54.264296],"type": "Point"},"properties": {"category": "online","name": "Castle Farm Fresh Produce","phone": "+447568436413","website": "https://www.castlefarmni.com/","storeid": "02"},"type": "Feature"}, {"geometry": {"coordinates": [-2.7721087, 53.9136975],"type": "Point"},"properties": {"category": "online","name": "Lancashire Honey","phone": "+447787758033","website": "http://www.lancashirehoney.com/","storeid": "06"},"type": "Feature"},]}'
+var geoObject = JSON.parse(geoJSONObject);
+var features = [];
+features = geoObject.features;
+console.log(features.name);*/
 // initilise Map
 
 function initMap() {
@@ -6,6 +13,8 @@ function initMap() {
     center: { lat: 55.08122, lng: -3.016536 },
     mapTypeControl: false,
   });
+
+    new AutocompleteDirectionsHandler(map);
 
   // set markers and clusters
 
@@ -163,3 +172,97 @@ $(".toggleList").click(function () {
 });
 
   
+function AutocompleteDirectionsHandler(map) {
+  this.map = map;
+  this.originPlaceId = null;
+  this.destinationPlaceId = null;
+  this.travelMode = 'WALKING';
+  this.directionsService = new google.maps.DirectionsService;
+  this.directionsRenderer = new google.maps.DirectionsRenderer;
+  this.directionsRenderer.setMap(map);
+
+  var originInput = document.getElementById('origin-input');
+  var destinationInput = document.getElementById('destination-input');
+  var modeSelector = document.getElementById('mode-selector');
+
+  var originAutocomplete = new google.maps.places.Autocomplete(originInput);
+  // Specify just the place data fields that you need.
+  originAutocomplete.setFields(['place_id']);
+
+  var destinationAutocomplete =
+      new google.maps.places.Autocomplete(destinationInput);
+  // Specify just the place data fields that you need.
+  destinationAutocomplete.setFields(['place_id']);
+
+  this.setupClickListener('changemode-walking', 'WALKING');
+  this.setupClickListener('changemode-transit', 'TRANSIT');
+  this.setupClickListener('changemode-driving', 'DRIVING');
+
+  this.setupPlaceChangedListener(originAutocomplete, 'ORIG');
+  this.setupPlaceChangedListener(destinationAutocomplete, 'DEST');
+
+  this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(originInput);
+  this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(
+      destinationInput);
+  this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(modeSelector);
+}
+
+// Sets a listener on a radio button to change the filter type on Places
+// Autocomplete.
+AutocompleteDirectionsHandler.prototype.setupClickListener = function(
+    id, mode) {
+  var radioButton = document.getElementById(id);
+  var me = this;
+
+  radioButton.addEventListener('click', function() {
+    me.travelMode = mode;
+    me.route();
+  });
+};
+
+AutocompleteDirectionsHandler.prototype.setupPlaceChangedListener = function(
+    autocomplete, mode) {
+  var me = this;
+  autocomplete.bindTo('bounds', this.map);
+
+  autocomplete.addListener('place_changed', function() {
+    var place = autocomplete.getPlace();
+
+    if (!place.place_id) {
+      window.alert('Please select an option from the dropdown list.');
+      return;
+    }
+    if (mode === 'ORIG') {
+      me.originPlaceId = place.place_id;
+    } else {
+      me.destinationPlaceId = place.place_id;
+    }
+    me.route();
+  });
+};
+
+AutocompleteDirectionsHandler.prototype.route = function() {
+  if (!this.originPlaceId || !this.destinationPlaceId) {
+    return;
+  }
+  var me = this;
+
+  this.directionsService.route(
+      {
+        origin: {'placeId': this.originPlaceId},
+        destination: {'placeId': this.destinationPlaceId},
+        travelMode: this.travelMode
+      },
+      function(response, status) {
+        if (status === 'OK') {
+          me.directionsRenderer.setDirections(response);
+        } else {
+          window.alert('Directions request failed due to ' + status);
+        }
+      });
+};
+
+
+let txt = '{"category": "online", "name": "Castle Farm Fresh Produce", "latitude":"-5.851389", "longitude":"54.264296", "phone": "+447568436413", "website": "https://www.castlefarmni.com/","storeid": "02"}'
+let obj = JSON.parse(txt);
+document.getElementById("demo").innerHTML = obj.latitude + obj.longitude;
