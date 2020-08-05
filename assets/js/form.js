7cc4c8eb6fddef1fa5f0708a6f6c45e436672d44
irahbt <irah.bt@gmail.com>
2020-08-05 16:22:42 +0000

    Add to modal index.html and css

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,7 +1,7 @@
 function sendForm(){
 emailjs.sendForm("gmail", "honey_locator", "#myForm")
     .then(function() {
-       alert("Thanks for your submission!");
+       alert("Thanks for your suggestion, we'll get back to you asap!");
     }, function(error) {
        alert("Hmm something has gone wrong here, please try again...", error);
     });
