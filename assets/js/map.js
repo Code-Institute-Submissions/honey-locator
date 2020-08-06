ea2ef844193cfecf2a4b8d8f9eb14d8842b62e9c
irahbt <irah.bt@gmail.com>
2020-08-06 13:37:28 +0000

    Fix autocomplete search by adding container id

--- assets/js/map.js
+++ assets/js/map.js
@@ -316,17 +316,14 @@ function initMap() {
   }
 
   //search bar autocomplete
-  let card = document.getElementById("pac-card");
+  let card = document.getElementById("pac-container");
   let input = document.getElementById("pac-input");
-  let types = document.getElementById("type-selector");
   let options = {
     types: ["address"],
     componentRestrictions: { country: "gb" },
   };
 
   const autocomplete = new google.maps.places.Autocomplete(input, options);
-
-  autocomplete.bindTo("bounds", map);
 
   autocomplete.setFields(["address_components", "geometry", "name"]);
 
@@ -420,6 +417,50 @@ $(".toggleList").click(function () {
     $this.html(`<i class="fas fa-plus-square"></i>`);
   }
 });
+
+//Make the DIV element draggagle:
+dragElement(document.getElementById("listDiv"));
+
+function dragElement(elmnt) {
+  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  if (document.getElementById(elmnt.id + "header")) {
+    /* if present, the header is where you move the DIV from:*/
+    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+  } else {
+    /* otherwise, move the DIV from anywhere inside the DIV:*/
+    elmnt.onmousedown = dragMouseDown;
+  }
+
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    // call a function whenever the cursor moves:
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    // set the element's new position:
+    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+  }
+
+  function closeDragElement() {
+    /* stop moving when mouse button is released:*/
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
 
 
 //----------benefits
