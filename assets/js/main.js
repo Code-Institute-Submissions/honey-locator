e4719f78940f5768bf79883bb579aecf9e7143fc
irahbt <irah.bt@gmail.com>
2020-09-10 11:53:27 +0000

    Change add-btn to heart and write instruction in infoWindow

--- assets/js/main.js
+++ assets/js/main.js
@@ -420,7 +420,9 @@ function showDetails(placeResult, marker, status) {
             <div class="py-1"><i class="fas fa-store honey-col" alt="store"></i> ${placeResult.formatted_address}</div>
         <div>
              <div class="py-1"><i class="fas fa-map-marked-alt honey-col" alt="map"></i><a class="content-text text-hvr" target="_blank" target="_blank" href="${placeResult.url}"> View on GoogleMaps</a>    
-        </div>`);
+        </div>
+        <div class="py-1">Save this location clicking <i class="fas fa-heart honey-col"></i> below!</div>
+        `);
 
     placeInfowindow.open(marker.map, marker);
     currentInfoWindow.close();
